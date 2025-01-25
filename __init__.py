c2f77034d92a00b188a23c187c0b2106dd50ebe9
RossSaab <rosssaab@live.com>
2025-01-25 21:29:14 +0000

    Initial commit: Basic Flask site setup

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from app.models import User
+        return User.query.get(int(user_id))
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app 
